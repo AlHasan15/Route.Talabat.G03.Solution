0cd347c8fae24a4b375535e7e52930f6f7412fc0
AlHasan15 <alhassanrasmy1@gmail.com>
2024-06-05 02:01:25 +0300

    Get Product By Id Endpoint

---
+++ Route.Talabat.APIs/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entites;
+using Talabat.Core.Repositories.Contract;
+
+namespace Route.Talabat.APIs.Controllers
+{
+	public class ProductsController : BaseApiController
+	{
+		private readonly IGenaricRepository<Product> productsRepo;
+
+		public ProductsController(IGenaricRepository<Product> productsRepo)
+        {
+			this.productsRepo = productsRepo;
+		}
+
+		// / api/products
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+		{
+			var products = await productsRepo.GetAllAsync();
+
+			///JsonResult result = new JsonResult(products);
+			///OkResult result = new OkResult(products);
+
+			return Ok(products);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Product>> GetProduct(int id)
+		{
+			var products = await productsRepo.GetAsync(id);
+
+			if (products is null)
+			{
+				return NotFound( new {message = "Not found" , statsCode = 404}); // 404 
+			}
+
+			return Ok(products); // 200 
+		}
+
+    }
+}
