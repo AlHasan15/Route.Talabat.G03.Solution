fa689f95a1f6076752dc9a844ff52a6c1cf51137
AlHasan15 <alhassanrasmy1@gmail.com>
2024-06-06 02:16:19 +0300

    Session05

--- Route.Talabat.APIs/Program.cs
+++ Route.Talabat.APIs/Program.cs
@@ -1,16 +1,24 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Route.Talabat.APIs.Errors;
 using Route.Talabat.APIs.Extensions;
 using Route.Talabat.APIs.Helpers;
 using Route.Talabat.APIs.Middlewares;
 using StackExchange.Redis;
+using System.Text;
+using Talabat.Application.AuthService;
 using Talabat.Core.Entites;
+using Talabat.Core.Entites.Identity;
 using Talabat.Core.Repositories.Contract;
+using Talabat.Core.Services.Contract;
 using Talabat.Infrastructure;
 using Talabat.Infrastructure.Data;
+using Talabat.Infrastructure.Identity;
 
 namespace Route.Talabat.APIs
 {
@@ -28,7 +36,10 @@ namespace Route.Talabat.APIs
 			#region Configure Service method from dot net 5 
 			// Add services to the container.
 
-			webApplicationBuilder.Services.AddControllers();
+			webApplicationBuilder.Services.AddControllers().AddNewtonsoftJson(options =>
+			{
+				options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+			});
 
 			webApplicationBuilder.Services.AddSwaggerServices();
 
@@ -47,33 +58,57 @@ namespace Route.Talabat.APIs
 
 			webApplicationBuilder.Services.AddApplicationServices();
 
+			webApplicationBuilder.Services.AddDbContext<ApplicationIdentityDbContext>(option =>
+			{
+				option.UseSqlServer(webApplicationBuilder.Configuration.GetConnectionString("IdentityConnection"));
+			});
+
+			webApplicationBuilder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+			{
+
+			}).AddEntityFrameworkStores<ApplicationIdentityDbContext>();
+
+
+
+			webApplicationBuilder.Services.AddAuthServices(webApplicationBuilder.Configuration);
+
+
 			#endregion
 
 			var app = webApplicationBuilder.Build();
 
+			#region apply all pending migrations [update-database] and data seeding
 			using var scop = app.Services.CreateScope();
 
 			var services = scop.ServiceProvider;
 
-			var _dbContext = services.GetRequiredService<StoreContext>();
-			// ask clr for creating object from dbcontext explicitly 
-
+			var _dbContext = services.GetRequiredService<StoreContext>();// ask clr for creating object from dbcontext explicitly 
+			var _IdentityDbContext = services.GetRequiredService<ApplicationIdentityDbContext>();// ask clr for creating object from dbcontext explicitly 
 			var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+			var logger = loggerFactory.CreateLogger<Program>();
+
+
+
 			try
 			{
-				await _dbContext.Database.MigrateAsync();
+				await _dbContext.Database.MigrateAsync();// update database 
 				await StoreContextSeed.SeedAsync(_dbContext);
+
+
+				await _IdentityDbContext.Database.MigrateAsync(); // update database 
+				var _userManger = services.GetRequiredService<UserManager<ApplicationUser>>();
+				await ApplicationIdentityContextSeed.SeedUserAsync(_userManger);
 			}
 			catch (Exception ex)
 			{
 				Console.WriteLine(ex);
 
-				var logger = loggerFactory.CreateLogger<Program>();
 				logger.LogError(ex.StackTrace, "an error has been occured during apply migration");
-			}       
+			}  
+			#endregion
 
 			#region configure() method to configure kestrel middlewares like dot net 5 
-	        
+
 			app.UseMiddleware<ExceptionMiddleware>();
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
